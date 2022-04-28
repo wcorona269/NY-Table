0697a77b8ed6c1f98d41d3c01347c01412b3a30f
wcorona269 <wcorona269@gmail.com>
2022-04-28 14:15:50 -0400

    session errors reducer

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -2,6 +2,17 @@ import * as SessionUtils from '../util/session_api_util';
 
 export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER'
 export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER'
+export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS'
+export const REMOVE_SESSION_ERRORS = 'REMOVE_SESSION_ERRORS'
+
+export const removeSessionErrors = () => ({
+    type: REMOVE_SESSION_ERRORS
+})
+
+export const receiveSessionErrors = errors => ({
+    type: RECEIVE_SESSION_ERRORS,
+    errors
+});
 
 export const receiveCurrentUser = user => ({
     type: RECEIVE_CURRENT_USER,
--- frontend/reducers/session_errors_reducer.js
+++ frontend/reducers/session_errors_reducer.js
@@ -1,7 +1,19 @@
-
+import {
+	RECEIVE_SESSION_ERRORS,
+	RECEIVE_CURRENT_USER,
+	REMOVE_SESSION_ERRORS
+} from "../actions/session_actions"
 
 const sessionErrorsReducer = (oldState = {}, action) {
 	Object.freeze(oldState);
 
-	switch(action.)
+	switch(action.type) {
+		case RECEIVE_SESSION_ERRORS:
+			return action.errors;
+		case RECEIVE_CURRENT_USER:
+			return [];
+		case REMOVE_SESSION_ERRORS:
+			return [];
+	}
 }
+
