212ee2d530184d982771890b95308aba2dd799f3
wcorona269 <wcorona269@gmail.com>
2022-05-06 14:30:47 -0400

    final push before presentation

--- frontend/components/nav_bar/about_dropdown.jsx
+++ frontend/components/nav_bar/about_dropdown.jsx
@@ -10,7 +10,7 @@ const AbtDropdown = ({abtDropdown}) => {
 			<div className="abt-drop-info-container">
 				My name is William Corona and I am a full-stack software engineering student at <p className="red-text">App Academy.</p>
 				<br />
-				<p className="red-text">MyTable</p> is an online web application for booking restaurant reservations. It is inspired by and modeled after <p className="red-text">OpenTable</p>.
+				<p className="red-text">NY Table</p> is an online web application for booking restaurant reservations in NYC. It is inspired by and modeled after <p className="red-text">OpenTable</p>.
 				<br />
 				This project uses <p className="blue-text">React</p>, <p className="blue-text">Redux</p>, <p className="blue-text">CSS</p> and <p className="blue-text">SASS</p> for frontend configuration, and <p className="blue-text">Rails/PostgreSQL</p> on the backend.
 			</div>
--- frontend/components/nav_bar/nav_left.jsx
+++ frontend/components/nav_bar/nav_left.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import { withRouter } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 
 const NavLeft = () => {
 	return (
 		<div className="nav-left">
-			<img className="nav-logo" src="https://i.imgur.com/TXAOA6P.png" alt="NY Table logo"/>
+			<Link to={'/'}>
+				<img className="nav-logo" src="https://i.imgur.com/TXAOA6P.png" alt="NY Table logo"/>
+			</Link>
 		</div>
 	);
 };
